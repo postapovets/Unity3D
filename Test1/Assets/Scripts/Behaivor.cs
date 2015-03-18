19b8ca1afb7e5a48cc057744c441bb611106835d
Petro Ostapovets <postapovets@gmail.com>
2015-03-18 09:16:31 +0200

    task1

---
+++ Test1/Assets/Scripts/Behaivor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Behaivor : MonoBehaviour
+{
+
+	Vector3 moveVector;
+	float rndSpriteTimer;
+	float timerSprite = 0;
+	public Sprite[] sprites = new Sprite[4];
+
+	// Use this for initialization
+	void Start ()
+	{
+		moveVector = new Vector3 (Random.Range (-0.01f, 0.01f), 
+		                          Random.Range (-0.01f, 0.01f), 
+		                          Random.Range (-0.01f, 0.01f));
+		rndSpriteTimer = Random.Range (1, 3);
+		GetComponent<SpriteRenderer> ().sprite = sprites [Random.Range (0, 4)];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		gameObject.transform.position += moveVector;
+
+		timerSprite += Time.deltaTime;
+		if (timerSprite > rndSpriteTimer) { // change sprite image
+			GetComponent<SpriteRenderer> ().sprite = sprites [Random.Range (0, 4)];
+			timerSprite = 0;
+		}
+
+	}
+
+	void OnMouseDown ()
+	{
+		if (GetComponent<SpriteRenderer> ().sprite.name == "spongebob") {
+			Task1.Counter += 3;
+			Destroy (gameObject);
+		} else {
+			Task1.Counter--;
+		}
+	}
+}
---
+++ Test1/Assets/Scripts/Task1.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Task1 : MonoBehaviour
+{
+	
+	float randomTime;
+	float timer1 = 0;
+	bool msgPrinted = false;
+	public GameObject pers;
+	public static float GameTimer = 20;
+	public static int Counter = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		randomTime = Random.Range (1f, 3f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (GameTimer > 0) {
+			GameTimer -= Time.deltaTime;
+			timer1 += Time.deltaTime;
+			if (timer1 > randomTime) {
+				randomTime = Random.Range (1f, 3f); // generate new random timer
+				timer1 = 0;
+
+				pers = (GameObject)Instantiate (pers, new Vector3 (Random.Range (-5f, 5f), 
+					                            Random.Range (-5f, 5f), 
+					                            Random.Range (-5f, 5f)), new Quaternion (0f, 0f, 0f, 0f));
+				pers.transform.parent = gameObject.transform;
+			}
+		} else if (! msgPrinted) {  // print message one time
+			Destroy (gameObject);
+			print ("Counter = " + Counter);
+			msgPrinted = true;
+		}
+	}
+	
+}
