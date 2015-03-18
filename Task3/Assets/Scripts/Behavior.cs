543ae185f84449a54b816f7901bbb6bd727eed1d
Petro Ostapovets <postapovets@gmail.com>
2015-03-18 19:59:19 +0200

    some logic update

--- Task3/Assets/Scripts/Behavior.cs
+++ Task3/Assets/Scripts/Behavior.cs
@@ -12,43 +12,89 @@ public class Behavior : MonoBehaviour
 		x = int.Parse (name.Substring (2, 2));
 		y = int.Parse (name.Substring (5, 2));
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
-	}
 
 	void OnMouseDown ()
 	{
 		bool isHavePair = false;
 
-		if (x - 1 >= 0 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [x - 1, y].GetComponent<SpriteRenderer> ().color) {
+		if (this.MarkLeft (x, y))
 			isHavePair = true;
-			Task3.gameField [x - 1, y].GetComponent<SpriteRenderer> ().color = Color.clear;
-		}
 
-		if (x + 1 < 15 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [x + 1, y].GetComponent<SpriteRenderer> ().color) {
+		if (this.MarkRight (x, y))
 			isHavePair = true;
-			Task3.gameField [x + 1, y].GetComponent<SpriteRenderer> ().color = Color.clear;
 			
-		}
+		if (this.MarkDown (x, y))
+			isHavePair = true;
 
-		if (y - 1 >= 0 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [x, y - 1].GetComponent<SpriteRenderer> ().color) {
+		if (this.MarkUp (x, y))
 			isHavePair = true;
-			Task3.gameField [x, y - 1].GetComponent<SpriteRenderer> ().color = Color.clear;
-			
-		}
-		
-		if (y + 1 < 25 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [x, y + 1].GetComponent<SpriteRenderer> ().color) {
-			isHavePair = true;
-			Task3.gameField [x, y + 1].GetComponent<SpriteRenderer> ().color = Color.clear;
-			
-		}
 
 		if (isHavePair) {
-			gameObject.GetComponent<SpriteRenderer> ().enabled = false;
-			Task3.SortField ();
+			gameObject.GetComponent<SpriteRenderer> ().color = Color.clear;
+			SortField ();
+		}
+	}
+
+	void SortField ()
+	{
+		for (int i = 0; i < 15; i++) {
+			//sort col
+			int k1 = 0;
+			int k2 = 24;
+			Color[] tmpColors = new Color[25];
+			for (int j = 0; j < 25; j++) {
+				if (Task3.gameField [i, j].GetComponent<SpriteRenderer> ().color.Equals (Color.clear)) {
+					tmpColors [k2] = Color.clear;
+					k2--;
+				} else {
+					tmpColors [k1] = Task3.gameField [i, j].GetComponent<SpriteRenderer> ().color;
+					k1++;
+				}
+			}
+			for (int j = 0; j < 25; j++) {
+				Task3.gameField [i, j].GetComponent<SpriteRenderer> ().color = tmpColors [j];
+			}
+		}
+	}
+
+	private bool MarkLeft (int i, int j)
+	{
+		if (i - 1 >= 0 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [i - 1, j].GetComponent<SpriteRenderer> ().color) {
+			this.MarkLeft (i - 1, j);
+			Task3.gameField [i - 1, j].GetComponent<SpriteRenderer> ().color = Color.clear;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	private bool MarkRight (int i, int j)
+	{
+		if (i + 1 < 15 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [i + 1, j].GetComponent<SpriteRenderer> ().color) {
+			this.MarkRight (i + 1, j);
+			Task3.gameField [i + 1, j].GetComponent<SpriteRenderer> ().color = Color.clear;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	private bool MarkDown (int i, int j)
+	{
+		if (j - 1 >= 0 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [i, j - 1].GetComponent<SpriteRenderer> ().color) {
+			this.MarkDown (i, j - 1);
+			Task3.gameField [i, j - 1].GetComponent<SpriteRenderer> ().color = Color.clear;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	private bool MarkUp (int i, int j)
+	{
+		if (j + 1 < 25 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [i, j + 1].GetComponent<SpriteRenderer> ().color) {
+			this.MarkUp (i, j + 1);
+			Task3.gameField [i, j + 1].GetComponent<SpriteRenderer> ().color = Color.clear;
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
--- Task3/Assets/Scripts/Task3.cs
+++ Task3/Assets/Scripts/Task3.cs
@@ -5,47 +5,22 @@ public class Task3 : MonoBehaviour
 {
 
 	// Use this for initialization
-	static int dimX = 15;
-	static int dimY = 25;
+	int dimX = 15;
+	int dimY = 25;
 	Color[] colors = {Color.white, Color.green, Color.red, Color.yellow, Color.cyan};
 
 	public GameObject circle;
 
-	public static GameObject[,] gameField = new GameObject[dimX, dimY];
+	public static GameObject[,] gameField;
 
 	void Start ()
 	{
+		gameField = new GameObject[dimX, dimY];
 		for (int i = 0; i < dimX; i++) {
 			for (int j = 0; j < dimY; j++) {
 				gameField [i, j] = (GameObject)Instantiate (circle, new Vector3 (i, j, 0f), new Quaternion (0, 0, 0, 0));
 				gameField [i, j].name = string.Format ("c_{0:00}_{1:00}", i, j);
 				gameField [i, j].GetComponent<SpriteRenderer> ().color = colors [Random.Range (0, 5)];
-			}
-		}
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		
-	}
-
-	public static void SortField ()
-	{
-		for (int i = 0; i < 15; i++) {
-			bool swapped = true;
-			while (swapped) {
-				swapped = false;
-				for (int j = 0; i < 24; j++) {
-					if (!gameField [i, j].GetComponent<SpriteRenderer> ().color.Equals (Color.clear) &&
-						gameField [i, j + 1].GetComponent<SpriteRenderer> ().color.Equals (Color.clear)) {
-						gameField [i, j + 1].GetComponent<SpriteRenderer> ().color = 
-							gameField [i, j].GetComponent<SpriteRenderer> ().color;
-
-						gameField [i, j].GetComponent<SpriteRenderer> ().color = Color.clear;
-						swapped = true;
-					}
-				}
 			}
 		}
 	}
