5c2b3eb889faa74128149317df2751b9140641d9
Petro Ostapovets <postapovets@gmail.com>
2015-03-18 10:46:51 +0200

    Task2

---
+++ Test2/Assets/Scripts/Behavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Behavior : MonoBehaviour
+{
+
+	// Use this for initialization
+	int i;
+	int j;
+	void Start ()
+	{
+		i = int.Parse (name.Substring (6, 1));
+		j = int.Parse (name.Substring (8, 1));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnMouseEnter ()
+	{
+		Colorize (Color.yellow, 2);
+		Colorize (Color.green, 1);
+		GetComponent<SpriteRenderer> ().color = Color.red;
+
+	}
+
+	void OnMouseExit ()
+	{
+		Colorize (Color.white, 2);
+	}
+
+	private void Colorize (Color color, int size)
+	{
+		for (int x = i-size; x <= i+size; x++) {
+			for (int y = j - size; y <= j+size; y++) {
+				if ((x >= 0 && x < 6) && (y >= 0 && y < 6)) {
+					Task2.squares [x, y].GetComponent<SpriteRenderer> ().color = color;
+				}
+			}
+		}
+	}
+}
---
+++ Test2/Assets/Scripts/Task2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Task2 : MonoBehaviour
+{
+
+	public GameObject obj;
+
+	public static GameObject[,] squares = new GameObject[6, 6];
+
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++) {
+				squares [i, j] = (GameObject)Instantiate (obj, new Vector3 (i - 3, j - 3, 0f), new Quaternion (0f, 0f, 0f, 0f));
+				squares [i, j].name = "square" + i + "_" + j;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
