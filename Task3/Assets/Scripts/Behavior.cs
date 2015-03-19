842221fae3ae69a7a808c24e0d1c36846e3bc50f
Petro Ostapovets <postapovets@gmail.com>
2015-03-19 18:14:19 +0200

    Almost done (ver 0.0.1a)

--- Task3/Assets/Scripts/Behavior.cs
+++ Task3/Assets/Scripts/Behavior.cs
@@ -1,100 +1,86 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Behavior : MonoBehaviour
 {
 	int x;
 	int y;
+	Color color;
+	List<GameObject> selectedObj = new List<GameObject> ();
 
 	// Use this for initialization
 	void Start ()
 	{
-		x = int.Parse (name.Substring (2, 2));
-		y = int.Parse (name.Substring (5, 2));
+		color = gameObject.GetComponent<SpriteRenderer> ().color;
 	}
 
 	void OnMouseDown ()
 	{
-		bool isHavePair = false;
+		x = int.Parse (name.Substring (2, 2));
+		y = int.Parse (name.Substring (5, 2));
 
-		if (this.MarkLeft (x, y))
-			isHavePair = true;
-
-		if (this.MarkRight (x, y))
-			isHavePair = true;
-			
-		if (this.MarkDown (x, y))
-			isHavePair = true;
-
-		if (this.MarkUp (x, y))
-			isHavePair = true;
-
-		if (isHavePair) {
-			gameObject.GetComponent<SpriteRenderer> ().color = Color.clear;
+		SelectBubbles (x, y);
+		if (selectedObj.Count > 1) {
+			Task3.score += selectedObj.Count; // TODO: update score text
+			foreach (var item in selectedObj) {
+				int itemX = int.Parse (item.name.Substring (2, 2));
+				int itemY = int.Parse (item.name.Substring (5, 2));
+				Destroy (Task3.gameField [itemX, itemY]);
+				Task3.gameField [itemX, itemY] = null;
+			}
 			SortField ();
+		}
+	}
+
+	void SelectBubbles (int i, int j)
+	{
+		if (Task3.gameField [i, j] != null) {
+			if (selectedObj.Contains (Task3.gameField [i, j]))
+				return;
+			if (Task3.gameField [i, j].GetComponent<SpriteRenderer> ().color == color) {
+				selectedObj.Add (Task3.gameField [i, j]);
+				if (i > 0)
+					SelectBubbles (i - 1, j);
+				if (j > 0)
+					SelectBubbles (i, j - 1);
+				if (i < 14)
+					SelectBubbles (i + 1, j);
+				if (j < 24)
+					SelectBubbles (i, j + 1);
+			}
 		}
 	}
 
 	void SortField ()
 	{
-		for (int i = 0; i < 15; i++) {
-			//sort col
+		for (int i = 0; i < 15; i++) { // sort columns
 			int k1 = 0;
 			int k2 = 24;
-			Color[] tmpColors = new Color[25];
+			GameObject[] tmpObjs = new GameObject[25];
 			for (int j = 0; j < 25; j++) {
-				if (Task3.gameField [i, j].GetComponent<SpriteRenderer> ().color.Equals (Color.clear)) {
-					tmpColors [k2] = Color.clear;
+				tmpObjs [j] = Task3.gameField [i, j];
+			}
+			for (int j = 0; j < 25; j++) {
+				if (tmpObjs [j] == null) {
+					Task3.gameField [i, k2] = null;
 					k2--;
 				} else {
-					tmpColors [k1] = Task3.gameField [i, j].GetComponent<SpriteRenderer> ().color;
+					Task3.gameField [i, k1] = tmpObjs [j];
+					Task3.gameField [i, k1].name = string.Format ("c_{0:00}_{1:00}", i, k1);
 					k1++;
 				}
 			}
-			for (int j = 0; j < 25; j++) {
-				Task3.gameField [i, j].GetComponent<SpriteRenderer> ().color = tmpColors [j];
+		}
+		// TODO: move empty columt
+/*		for (int i = 0; i < 15; i++) {
+			if (Task3.gameField [i + 1, 0] == null) {
+				for (int j = 0; j < 25; j++) {
+					Task3.gameField [i + 1, j] = Task3.gameField [i, j];
+					Task3.gameField [i + 1, j].name = string.Format ("c_{0:00}_{1:00}", i + 1, j);
+				}
 			}
-		}
-	}
-
-	private bool MarkLeft (int i, int j)
-	{
-		if (i - 1 >= 0 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [i - 1, j].GetComponent<SpriteRenderer> ().color) {
-			this.MarkLeft (i - 1, j);
-			Task3.gameField [i - 1, j].GetComponent<SpriteRenderer> ().color = Color.clear;
-			return true;
-		} else {
-			return false;
-		}
-	}
-	private bool MarkRight (int i, int j)
-	{
-		if (i + 1 < 15 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [i + 1, j].GetComponent<SpriteRenderer> ().color) {
-			this.MarkRight (i + 1, j);
-			Task3.gameField [i + 1, j].GetComponent<SpriteRenderer> ().color = Color.clear;
-			return true;
-		} else {
-			return false;
-		}
-	}
-	private bool MarkDown (int i, int j)
-	{
-		if (j - 1 >= 0 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [i, j - 1].GetComponent<SpriteRenderer> ().color) {
-			this.MarkDown (i, j - 1);
-			Task3.gameField [i, j - 1].GetComponent<SpriteRenderer> ().color = Color.clear;
-			return true;
-		} else {
-			return false;
-		}
-	}
-	private bool MarkUp (int i, int j)
-	{
-		if (j + 1 < 25 && gameObject.GetComponent<SpriteRenderer> ().color == Task3.gameField [i, j + 1].GetComponent<SpriteRenderer> ().color) {
-			this.MarkUp (i, j + 1);
-			Task3.gameField [i, j + 1].GetComponent<SpriteRenderer> ().color = Color.clear;
-			return true;
-		} else {
-			return false;
-		}
+			// TODO: move GameObject p with childs to right (x+1)
+		}*/
 	}
 }
--- Task3/Assets/Scripts/Task3.cs
+++ Task3/Assets/Scripts/Task3.cs
@@ -11,16 +11,20 @@ public class Task3 : MonoBehaviour
 
 	public GameObject circle;
 
-	public static GameObject[,] gameField;
+	//TODO: remove static fields
+	public static GameObject[,] gameField;  
+	public static int score = 0;
 
 	void Start ()
 	{
 		gameField = new GameObject[dimX, dimY];
 		for (int i = 0; i < dimX; i++) {
+			GameObject p = new GameObject ("p" + i);
 			for (int j = 0; j < dimY; j++) {
 				gameField [i, j] = (GameObject)Instantiate (circle, new Vector3 (i, j, 0f), new Quaternion (0, 0, 0, 0));
 				gameField [i, j].name = string.Format ("c_{0:00}_{1:00}", i, j);
 				gameField [i, j].GetComponent<SpriteRenderer> ().color = colors [Random.Range (0, 5)];
+				gameField [i, j].transform.parent = p.transform;
 			}
 		}
 	}
