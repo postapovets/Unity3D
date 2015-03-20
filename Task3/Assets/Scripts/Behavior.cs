410a5f13e62fe64aa572648ad8bdae92e8b3b5d0
Petro Ostapovets <postapovets@gmail.com>
2015-03-20 11:40:17 +0200

    Task3 ver 0.0.3a

---
+++ Task3/Assets/Scripts/Behavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Behavior : MonoBehaviour
+{
+	Task3 mainView;
+
+	// Use this for initialization
+	void Start ()
+	{
+		mainView = transform.root.GetComponent<Task3> ();
+	}
+
+	void OnMouseDown ()
+	{
+		mainView.DestroyBubbles (int.Parse (name.Substring (2, 2)),
+		                         int.Parse (name.Substring (5, 2)),
+		                         gameObject.GetComponent<SpriteRenderer> ().color);
+	}
+}
