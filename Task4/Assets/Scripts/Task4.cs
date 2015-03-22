db144ab26bb06ca8b16d4c00c2091b2272f05f67
Petro Ostapovets <postapovets@gmail.com>
2015-03-22 19:05:42 +0200

    Task4 v0.0.1
    LookAt and MoveTowards

---
+++ Task4/Assets/Scripts/Task4.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Task4 : MonoBehaviour
+{
+
+
+	public GameObject Cube;
+	public GameObject Sphere;
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Cube.transform.LookAt (Sphere.transform.position);
+		Cube.transform.position = Vector3.MoveTowards (Cube.transform.position, Sphere.transform.position, 0.05f);
+	}
+}
