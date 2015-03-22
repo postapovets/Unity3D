b6d26aa73b2deead90f982882473283d50200067
Petro Ostapovets <postapovets@gmail.com>
2015-03-22 18:45:32 +0200

    Task5: v.0.0.1

---
+++ Task5/Assets/Scripts/Shape.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shape : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnMouseDown ()
+	{
+		switch (transform.tag) {
+		case "circle":
+			transform.parent = GameObject.Find ("/Circles").transform;
+			transform.name = "circle_" + GameObject.Find ("/Circles").transform.childCount;
+			GetComponent<SpriteRenderer> ().color = Color.green;
+			break;
+		case "cube":
+			transform.parent = GameObject.Find ("/Cubes").transform;
+			transform.name = "cube_" + GameObject.Find ("/Cubes").transform.childCount;
+			GetComponent<SpriteRenderer> ().color = Color.green;
+			break;
+		case "triangle":
+			transform.parent = GameObject.Find ("/Triangles").transform;
+			transform.name = "triangle_" + GameObject.Find ("/Triangles").transform.childCount;
+			GetComponent<SpriteRenderer> ().color = Color.green;
+			break;
+		}
+		if (GameObject.Find ("/Unsorted").transform.childCount == 0) {
+			foreach (Transform child in GameObject.Find ("/Circles").transform) {
+				if (child.tag != "circle")
+					Debug.LogWarning ("Object " + child.name + " is not a circle.");
+			}
+			foreach (Transform child in GameObject.Find ("/Cubes").transform) {
+				if (child.tag != "cube")
+					Debug.LogWarning ("Object " + child.name + " is not a cube.");
+			}
+			foreach (Transform child in GameObject.Find ("/Triangles").transform) {
+				if (child.tag != "triangle")
+					Debug.LogWarning ("Object " + child.name + " is not a triangle.");
+			}
+		}
+	}
+}
---
+++ Task5/Assets/Scripts/Task5.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Task5 : MonoBehaviour
+{
+	public Sprite[] Shapes = new Sprite[3];
+	public GameObject GObj;
+	GameObject obj;
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i = 0; i < 10; i++) {
+			int s = Random.Range (0, 3);
+			obj = (GameObject)Instantiate (GObj, new Vector3 (Random.Range (-3f, 3f), 
+			                                       Random.Range (-3f, 3f), 
+			                                       Random.Range (-3f, 3f)), new Quaternion (0f, 0f, 0f, 0f));
+			obj.GetComponent<SpriteRenderer> ().sprite = Shapes [s];
+			obj.transform.parent = transform;
+			switch (s) {
+			case 0:
+				obj.tag = "circle";
+				break;
+			case 1:
+				obj.tag = "cube";
+				break;
+			case 2:
+				obj.tag = "triangle";
+				break;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
