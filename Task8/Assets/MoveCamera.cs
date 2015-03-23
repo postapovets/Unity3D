2763711a93b686c6207c78a682f9d396b613bb89
Petro Ostapovets <postapovets@gmail.com>
2015-03-23 12:36:59 +0200

    Task8: ver 0.0.1

---
+++ Task8/Assets/MoveCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position += new Vector3 (0, 0, Input.GetAxis ("Mouse ScrollWheel"));
+		if (Input.GetMouseButton (0)) {
+			transform.position += new Vector3 (Input.GetAxis ("Mouse X"), Input.GetAxis ("Mouse Y"), 0);
+		}
+	}
+}
