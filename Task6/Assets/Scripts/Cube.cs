34505f3410e4dc1e143a3717824bb9b5d3780a3e
Petro Ostapovets <postapovets@gmail.com>
2015-03-23 10:52:56 +0200

    Task6: ver 0.0.1

---
+++ Task6/Assets/Scripts/Cube.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour
+{
+	int speed = 30;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.RightShift))
+			speed = 10;
+		else if (Input.GetKey (KeyCode.RightAlt))
+			speed = 50;
+		else
+			speed = 30;
+		transform.position += new Vector3 (Input.GetAxis ("HorizontalCube") / speed, Input.GetAxis ("VerticalCube") / speed, 0);
+	}
+}
---
+++ Task6/Assets/Scripts/Sphere.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphere : MonoBehaviour
+{
+	int speed = 30;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.LeftShift))
+			speed = 10;
+		else if (Input.GetKey (KeyCode.LeftAlt))
+			speed = 50;
+		else
+			speed = 30;
+		transform.position += new Vector3 (Input.GetAxis ("HorizontalSphere") / speed, Input.GetAxis ("VerticalSphere") / speed, 0);
+	}
+}
