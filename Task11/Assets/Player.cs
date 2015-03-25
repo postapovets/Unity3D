00b68de7470e1f369de6cd9aa0cef59ad325dbe7
Petro Ostapovets <postapovets@gmail.com>
2015-03-25 10:38:56 +0200

    Task11: ver 0.0.1

---
+++ Task11/Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+	// Use this for initialization
+	public Color destColor;
+	void Start ()
+	{
+		destColor = transform.GetComponent<MeshRenderer> ().material.color;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.GetComponent<MeshRenderer> ().material.color = Color.Lerp (transform.GetComponent<MeshRenderer> ().material.color, destColor, 0.1f);
+	}
+}
---
+++ Task11/Assets/scrCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrCamera : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit rcHit;
+			if (Physics.Raycast (ray, out rcHit, 20f)) {
+
+				rcHit.collider.GetComponent<Player> ().destColor = new Color (Random.Range (0, 1f), Random.Range (0, 1f), Random.Range (0, 1f));
+			}
+		}
+	}
+}
