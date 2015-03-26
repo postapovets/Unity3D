405db3cc9eff22328c9bf9b14157b1b9f512c179
Petro Ostapovets <postapovets@gmail.com>
2015-03-26 15:21:12 +0200

    Game2Mazed

---
+++ Game2Mazed/Assets/Scripts/Mover.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+	public Rigidbody2D PlayerL;
+	public Rigidbody2D PlayerR;
+	public Rigidbody2D shot;
+	public int speed;
+	Vector2 shotDirection;
+
+	void Start ()
+	{
+		shotDirection = new Vector2 (1f, 0);
+	}
+
+	void FixedUpdate ()
+	{
+		if (Mathf.Abs (Input.GetAxis ("Horizontal")) > 0) {
+			if (Input.GetAxis ("Horizontal") < 0) {
+				shotDirection = new Vector2 (-1f, shotDirection.y);
+				PlayerL.transform.rotation = new Quaternion (0, 0, 0, 0);
+				PlayerR.transform.rotation = new Quaternion (0, 180f, 0, 0);
+			} else {
+				shotDirection = new Vector2 (1f, shotDirection.y);
+				PlayerL.transform.rotation = new Quaternion (0, 180f, 0, 0);
+				PlayerR.transform.rotation = new Quaternion (0, 0, 0, 0);
+			}
+			PlayerL.velocity = new Vector2 (Input.GetAxis ("Horizontal") * speed, PlayerL.velocity.y);
+			PlayerR.velocity = new Vector2 (-Input.GetAxis ("Horizontal") * speed, PlayerR.velocity.y);
+		}
+		if (Mathf.Abs (Input.GetAxis ("Vertical")) > 0) {
+			if (Input.GetAxis ("Vertical") > 0) {
+				shotDirection = new Vector2 (shotDirection.x, 1f);
+			} else {
+				shotDirection = new Vector2 (shotDirection.x, -1f);
+			}
+			PlayerL.velocity = new Vector2 (PlayerL.velocity.x, Input.GetAxis ("Vertical") * speed);
+			PlayerR.velocity = new Vector2 (PlayerR.velocity.x, Input.GetAxis ("Vertical") * speed);
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GameObject obj = (GameObject)Instantiate (shot, PlayerL.position, new Quaternion (0, 0, 0, 0));
+			obj.GetComponent<Shot> ().Direction = shotDirection;
+		}
+	}
+}
---
+++ Game2Mazed/Assets/Scripts/Shot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour
+{
+	private float shotTime;
+	public float Speed;
+	public Vector2 Direction;
+
+	// Use this for initialization
+	void Start ()
+	{
+		shotTime = Time.time;
+		gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (1f * Speed, 0);
+
+	}
+
+	void FixedUpdate ()
+	{
+		if (Time.time > (shotTime + 0.5f))
+			Destroy (gameObject);
+	}
+
+}
