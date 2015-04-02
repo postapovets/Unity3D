e5977f66f444ed1a07a650344443e054fc77ce21
Petro Ostapovets <postapovets@gmail.com>
2015-04-02 15:25:50 +0300

    Game_3InLine ver 0.0.3

--- Game_3InLine/Assets/Scripts/GameField.cs
+++ Game_3InLine/Assets/Scripts/GameField.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameField : MonoBehaviour
 {
@@ -11,19 +12,17 @@ public class GameField : MonoBehaviour
 
 	GameObject[,] gameField;
 	GameObject selected1 = null;
+	List<GameObject> selectedObjs = new List<GameObject> ();
+	bool needOpimization = true;
 
 
 	// Use this for initialization
 	void Start ()
 	{
 		gameField = new GameObject[dimX, dimY];
-		for (int i = 0; i < dimX; i++) {
-			for (int j = 0; j < dimY; j++) {
-				gameField [i, j] = (GameObject)Instantiate (perfabCircle, new Vector3 (i, j, 0), new Quaternion (0, 0, 0, 0));
-				gameField [i, j].name = "c_" + i + "_" + j;
-				gameField [i, j].transform.parent = gameObject.transform;
-				gameField [i, j].GetComponent<MeshRenderer> ().material.color = colors [Random.Range (0, 5)];
-			}
+		CreateNew ();
+		while (needOpimization) {
+			needOpimization = DropCirles ();
 		}
 	}
 	
@@ -42,6 +41,10 @@ public class GameField : MonoBehaviour
 						selected1 = hit.collider.gameObject;                                          
 					} else {
 						if (Vector3.Distance (selected1.transform.position, hit.collider.transform.position) == 1) {
+							// stop rotating animation
+							iTween.Stop (selected1);
+							iTween.Stop (hit.collider.gameObject);
+
 							Vector3[] path = new Vector3[] {
 							new Vector3 (selected1.transform.position.x, selected1.transform.position.y, selected1.transform.position.z - 1),
 							hit.collider.transform.position};
@@ -51,7 +54,7 @@ public class GameField : MonoBehaviour
 							new Vector3 (hit.collider.transform.position.x, hit.collider.transform.position.y, hit.collider.transform.position.z + 1),
 							selected1.transform.position};
 							iTween.MoveTo (hit.collider.gameObject, iTween.Hash ("path", path, "easetype", iTween.EaseType.linear, 
-							                                                     "oncomplete", "stopAnimation", "oncompletetarget", hit.collider.transform.parent.gameObject));
+							                                                     "oncomplete", "DropCirles", "oncompletetarget", hit.collider.transform.parent.gameObject));
 							// update array
 							for (int i = 0; i < dimX; i++) {
 								for (int j = 0; j < dimY; j++) {
@@ -61,42 +64,109 @@ public class GameField : MonoBehaviour
 										gameField [i, j] = selected1;
 								}
 							}
-							DropCirles ();
 						} else {
 							stopAnimation ();
 						}
+						iTween.Stop (selected1);
 						selected1 = null;
 					}
 				}
 			}
 		}
 	}
+	
 
 	void stopAnimation ()
 	{
-		iTween.Stop (gameObject, true);
+		//	iTween.Stop (gameObject, true);
 	}
 
-	void DropCirles ()
+	void SelectForDrop ()
 	{
 		for (int i = 0; i < dimX; i++) {
 			for (int j = 0; j < dimY; j++) {
 				Color myColor = gameField [i, j].GetComponent<MeshRenderer> ().material.color;
 				if (j < dimY - 2) {
 					if (gameField [i, j + 1].GetComponent<MeshRenderer> ().material.color == myColor && gameField [i, j + 2].GetComponent<MeshRenderer> ().material.color == myColor) {
-						iTween.RotateAdd (gameField [i, j], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
-						iTween.RotateAdd (gameField [i, j + 1], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
-						iTween.RotateAdd (gameField [i, j + 2], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
+						if (! selectedObjs.Contains (gameField [i, j]))
+							selectedObjs.Add (gameField [i, j]);
+						if (! selectedObjs.Contains (gameField [i, j + 1]))
+							selectedObjs.Add (gameField [i, j + 1]);
+						if (! selectedObjs.Contains (gameField [i, j + 2]))
+							selectedObjs.Add (gameField [i, j + 2]);
 					}
 				}
 				if (i < dimX - 2) {
 					if (gameField [i + 1, j].GetComponent<MeshRenderer> ().material.color == myColor && gameField [i + 2, j].GetComponent<MeshRenderer> ().material.color == myColor) {
-						iTween.RotateAdd (gameField [i, j], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
-						iTween.RotateAdd (gameField [i + 1, j], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
-						iTween.RotateAdd (gameField [i + 2, j], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
+						if (! selectedObjs.Contains (gameField [i, j]))
+							selectedObjs.Add (gameField [i, j]);
+						if (! selectedObjs.Contains (gameField [i + 1, j]))
+							selectedObjs.Add (gameField [i + 1, j]);
+						if (! selectedObjs.Contains (gameField [i + 2, j]))
+							selectedObjs.Add (gameField [i + 2, j]);
 					}	
 				}
 			}
 		}
 	}
+
+
+
+	bool DropCirles ()
+	{
+		stopAnimation ();
+		
+		SelectForDrop ();
+		bool result = selectedObjs.Count > 0;
+
+		//drop
+		for (int i = 0; i < dimX; i++) {
+			for (int j = 0; j < dimY; j++) {
+				if (selectedObjs.Contains (gameField [i, j])) {
+					selectedObjs.Remove (gameField [i, j]);
+					Destroy (gameField [i, j]);
+					gameField [i, j] = null;
+				}
+			}
+		}
+
+
+		CreateNew ();
+		
+		return result;
+	}
+
+	void CreateNew ()
+	{
+		for (int i = 0; i < dimX; i++) {
+			for (int j = 0; j < dimY; j++) {
+				if (gameField [i, j] == null) {
+					gameField [i, j] = (GameObject)Instantiate (perfabCircle, new Vector3 (i, j, 0), new Quaternion (0, 0, 0, 0));
+					gameField [i, j].transform.parent = gameObject.transform;
+					gameField [i, j].GetComponent<MeshRenderer> ().material.color = colors [Random.Range (0, 5)];
+					iTween.ScaleFrom (gameField [i, j], iTween.Hash ("scale", new Vector3 (0.1f, 0.1f, 0.1f), "oncomplete", "DropCirles", "oncompletetarget", gameField [i, j].transform.parent.gameObject));
+				}
+			}
+		}
+	}
+
+	void OptimizeArray ()
+	{
+		/*
+		string str = "";
+		//optimize
+		for (int i = 0; i < dimX; i++) {
+			for (int j = 0; j < dimY; j++) {
+				if (gameField [i, j] != null)
+					str += "1 ";
+				else
+					str += "0 ";
+			}
+			str += "\n";
+			
+		}
+		print (str);
+		*/
+	}
+
 }
