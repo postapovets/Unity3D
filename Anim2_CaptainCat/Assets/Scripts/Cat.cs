39e5977896da7eddd871f159113dadb1e702a5aa
Petro Ostapovets <postapovets@gmail.com>
2015-04-03 13:44:46 +0300

    Anim2

---
+++ Anim2_CaptainCat/Assets/Scripts/Cat.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat : MonoBehaviour
+{
+
+	public Transform groundSensor;
+	public LayerMask groundLayer;
+
+	bool onGround = false;
+	Rigidbody2D myBody;
+	Animator myAnim;
+	bool directionR = true;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		myBody = GetComponent<Rigidbody2D> ();
+		myAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (onGround && Input.GetKeyDown (KeyCode.Space)) {
+			onGround = false;
+			myBody.velocity = new Vector2 (myBody.velocity.x, 9);
+		}
+		if (myBody.transform.position.y < -3) {
+			myBody.position = new Vector2 (5, 1);
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		onGround = Physics2D.OverlapCircle (groundSensor.position, 0.3f, groundLayer);
+		myAnim.SetBool ("OnGround", onGround);
+		myAnim.SetFloat ("SpeedY", myBody.velocity.y);
+		float acceleration = Input.GetAxis ("Horizontal");
+		myAnim.SetFloat ("SpeedX", Mathf.Abs (acceleration));
+		myBody.velocity = new Vector2 (acceleration * 7, myBody.velocity.y);
+		if (directionR && acceleration < 0) {
+			Vector3 tmpScale = myBody.transform.localScale;
+			tmpScale.x *= -1;
+			myBody.transform.localScale = tmpScale;
+			directionR = !directionR;
+		} else if (!directionR && acceleration > 0) {
+			Vector3 tmpScale = myBody.transform.localScale;
+			tmpScale.x *= -1;
+			myBody.transform.localScale = tmpScale;
+			directionR = !directionR;
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+	}
+}
---
+++ Anim2_CaptainCat/Assets/Scripts/Coin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		iTween.RotateAdd (gameObject, iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Player") {
+			Vector3[] path = new Vector3[] {
+				new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z - 1),
+				new Vector3 (transform.position.x, transform.position.y, transform.position.z - 1),
+				new Vector3 (transform.position.x, -3, transform.position.z + 1)};
+			iTween.MoveTo (gameObject, iTween.Hash ("path", path, "easetype", iTween.EaseType.linear, "time", 2, "oncomplete", "KillSelf"));
+			GetComponent<Collider2D> ().enabled = false;
+		}
+	}
+
+	void KillSelf ()
+	{
+		Destroy (gameObject);
+	}
+}
