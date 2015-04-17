811d594f70ad6785d129144422f25c99221c8aef
Petro Ostapovets <postapovets@gmail.com>
2015-04-17 13:09:20 +0300

    Lessons

---
+++ Lessons/Assets/Scripts/MazeAlarm/BlinkLight.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkLight : MonoBehaviour
+{
+	float angleDirection = 1f;
+	void Update ()
+	{
+		transform.Rotate (0f, Time.deltaTime * angleDirection * 200f, 0f);
+	}
+}
---
+++ Lessons/Assets/Scripts/MazeAlarm/Detector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Detector : MonoBehaviour
+{
+
+	public GameObject AlarmLight;
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player") {
+			if (!AlarmLight.activeSelf) {
+				AlarmLight.SetActive (true);
+			}
+		}
+	}
+}
