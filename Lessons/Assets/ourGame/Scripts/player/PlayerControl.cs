7a1307894824c00bde5ca0c28ddec7cde219d1f6
Petro Ostapovets <postapovets@gmail.com>
2015-04-22 15:19:20 +0300

    lessons

---
+++ Lessons/Assets/ourGame/Scripts/player/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public speed;
+
+	private Transform player;
+
+	bool isGrounded = true;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	void FixedUpdate () {
+		float v = Input.GetAxis("Vertical");
+
+		if (isGrounded) {
+
+		}
+
+		//update animator
+
+	}
+}
