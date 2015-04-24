587cdaa25cafa5bdef60ff1e7cfbb0e82d1a89f8
Petro Ostapovets <postapovets@gmail.com>
2015-04-24 13:43:33 +0300

    Android Maze3D

--- Lessons/Assets/Scripts/Android/androidMove.cs
+++ Lessons/Assets/Scripts/Android/androidMove.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class androidMove : MonoBehaviour
 {
-	public GameObject camDir;
-	public GameObject camRot;
+	public Transform camDir;
+	public Transform camRot;
 
 	Vector3 zoomMin = new Vector3 (0, 3.3f, -5f);
 	Vector3 zoomMax = new Vector3 (0, 13f, -20f);
@@ -14,30 +14,38 @@ public class androidMove : MonoBehaviour
 
 	bool isZoom = false;
 
-
 	Rigidbody myBody;
 
 	void Start ()
 	{
 		myBody = GetComponent<Rigidbody> ();
 		Camera.main.transform.localPosition = Vector3.Lerp (zoomMin, zoomMax, t);
+
 	}
 	
 	void FixedUpdate ()
 	{
-		camDir.transform.position = transform.position;
+		camDir.position = transform.position;
 		Vector3 accel = Input.acceleration;
+		myBody.AddForce (camDir.TransformDirection (accel.x * 40, 0, (accel.y + 0.0f) * 60));
 
-		myBody.AddForce (camDir.transform.TransformDirection (accel.x * 40, 0, (accel.y + 0.0f) * 60));
-		// Camera moving
+		// Rotate camera
 		if (Input.touchCount == 1) {
 			if (Input.GetTouch (0).phase == TouchPhase.Moved) {
-				camDir.transform.Rotate (0, Input.GetTouch (0).deltaPosition.x * 0.5f, 0, Space.World);
-				camRot.transform.Rotate (-Input.GetTouch (0).deltaPosition.y * 0.5f, 0, 0, Space.Self);
+				camDir.Rotate (0, Input.GetTouch (0).deltaPosition.x * 0.3f, 0, Space.World);
+				camRot.Rotate (-Input.GetTouch (0).deltaPosition.y * 0.2f, 0, 0, Space.Self);
+				
+				if (camRot.localRotation.x < -0.2f) {
+					camRot.localRotation = new Quaternion (-0.2f, 0f, 0f, 1f);
+				}
+				if (camRot.localRotation.x > 0.35f) {
+					camRot.localRotation = new Quaternion (0.4f, 0f, 0f, 1f);
+				}
+
 			}
 		}
 
-		// Camera zoom
+		// Zoom camera
 		if (Input.touchCount == 2) {
 			if (Input.GetTouch (0).phase == TouchPhase.Moved || Input.GetTouch (1).phase == TouchPhase.Moved) {
 				if (isZoom) {
@@ -49,10 +57,7 @@ public class androidMove : MonoBehaviour
 					if (prevDist < dist) { //zoom in
 						t -= prevDist / dist * 0.05f;
 					}
-					if (t > 1)
-						t = 1f;
-					if (t < 0)
-						t = 0f;
+					t = Mathf.Clamp (t, 0f, 1f);
 
 					Camera.main.transform.localPosition = Vector3.Lerp (zoomMin, zoomMax, t);
 				} else {
