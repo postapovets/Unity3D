719aa16fd7a992043d7b5ea75d00eb8ba976dea3
Petro Ostapovets <postapovets@gmail.com>
2015-04-30 13:40:14 +0300

    UI

---
+++ Lessons/Assets/myUI/myUIMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+
+public class myUIMenu : MonoBehaviour
+{
+
+	Animator myAnim;
+	// Use this for initialization
+	void Start ()
+	{
+		myAnim = GetComponent<Animator> ();
+	}
+
+	public void btnPlayClick ()
+	{
+		btnExitClick ();
+		print ("Play");
+	}
+
+	public void btnExitClick ()
+	{
+		myAnim.SetBool ("MainMenuOpen", false);
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (!myAnim.GetBool ("MainMenuOpen")) {
+				myAnim.SetBool ("MainMenuOpen", true);
+
+			}
+		}
+	}
+
+	public void btnSettingsClick ()
+	{
+		myAnim.SetBool ("SettingsOpen", true);
+	}
+
+	public void btnCloseClick ()
+	{
+		myAnim.SetBool ("SettingsOpen", false);
+	}
+}
